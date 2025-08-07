b51f03fb7de3c7ad7efb1d1a643d137df0a65347
aszkudelski <a.szkudelski@hqnetworks.pl>
2025-08-07 14:39:34 +0200

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import os
 
 # Adjust these values as needed
-bind = "0.0.0.0:8000"  # Host and port for Gunicorn to listen on
-workers = 3  # The number of worker processes for concurrency (adjust based on system resources)
+bind = "0.0.0.0:21137"  # Host and port for Gunicorn to listen on
+workers = 5  # The number of worker processes for concurrency (adjust based on system resources)
 activate_base = True  # Activate your virtual environment if applicable
 
 # Path to your Django project's main WSGI application file (usually manage.py)
